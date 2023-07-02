d82bfde0687f1ddb04ad5ae783e30fd3206116b9
wilsonz0 <105890511+wilsonz0@users.noreply.github.com>
2023-07-01 21:18:45 -0400

    constructed the visual board

--- src/game/Board.java
+++ src/game/Board.java
@@ -2,27 +2,56 @@ package game;
 
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
 // will represent the Tic-Tac-Toe Board
 public class Board {
-	private String[][] board;
+	private Tile[][] board = new Tile[3][3];;
 	private StackPane pane;
 	
-	private Label test = new Label("This is a test message");
-	
 	public Board() {
-		board = new String[3][3];
-		
 		pane = new StackPane();
 		pane.setMinSize(Constants.APP_WIDTH, Constants.BOARD_HEIGHT);
 		pane.setTranslateX(Constants.APP_WIDTH / 2);
 		pane.setTranslateY((Constants.BOARD_HEIGHT / 2) + Constants.TOP_HEIGHT);
 		
-		test.setMinSize(Constants.APP_WIDTH, Constants.BOARD_HEIGHT);
-		pane.getChildren().add(test);
+		addAllTiles();
+	}
+	
+	private void addAllTiles() {
+		for (int row = 0; row < 3; row++) {
+			for (int col = 0; col < 3; col++) {
+				Tile tile = new Tile();
+				tile.getStackPane().setTranslateX((col * Constants.tileWidth) - Constants.tileWidth);
+				tile.getStackPane().setTranslateY((row * Constants.tileHeight) - Constants.tileHeight);
+				pane.getChildren().add(tile.getStackPane());
+				
+				board[row][col] = tile;
+			}
+		}
 	}
 	
 	public StackPane getStackPane() {
 		return pane;
+	}
+	
+	
+	private class Tile {
+		private StackPane pane;
+		
+		public Tile() {			
+			pane = new StackPane();
+			pane.setMinSize(Constants.tileWidth, Constants.tileHeight);
+			
+			Rectangle border = new Rectangle(Constants.tileWidth, Constants.tileHeight);
+			border.setFill(Color.TRANSPARENT);
+			border.setStroke(Color.BLACK);
+			pane.getChildren().add(border);
+		}
+		
+		public StackPane getStackPane() {
+			return pane;
+		}
 	}
 }
--- src/game/Constants.java
+++ src/game/Constants.java
@@ -5,4 +5,7 @@ public class Constants {
 	public static final int APP_HEIGHT = 500;
 	public static final int TOP_HEIGHT = 100;
 	public static final int BOARD_HEIGHT = 400;
+	
+	public static final double tileWidth = Constants.APP_WIDTH / 3 - 50;
+	public static final double tileHeight = Constants.BOARD_HEIGHT / 3 - 20;
 }
